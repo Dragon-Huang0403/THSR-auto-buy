c72f4bdb3e40166ff15850bf801b7bfecdfdc9bb
Dragon <dragon.huang@chelpis.com>
2023-01-12 08:30:16 +0800

    feat: rewrite train-item style

--- src/pages/time/search.tsx
+++ src/pages/time/search.tsx
@@ -1,49 +1,21 @@
-import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined';
-import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
-import {
-  Box,
-  Collapse,
-  IconButton,
-  Link,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Typography,
-} from '@mui/material';
+import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
+import DirectionsTransitRoundedIcon from '@mui/icons-material/DirectionsTransitRounded';
+import EastRoundedIcon from '@mui/icons-material/EastRounded';
+import KeyboardDoubleArrowRightRoundedIcon from '@mui/icons-material/KeyboardDoubleArrowRightRounded';
+import RouteRoundedIcon from '@mui/icons-material/RouteRounded';
+import ScheduleRoundedIcon from '@mui/icons-material/ScheduleRounded';
+import { Box, IconButton, Link, Typography } from '@mui/material';
 import { createProxySSGHelpers } from '@trpc/react-query/ssg';
 import type {
   GetServerSidePropsContext,
   InferGetServerSidePropsType,
 } from 'next';
-import React, { useReducer } from 'react';
+import React from 'react';
 import superjson from 'superjson';
 
-import type { TrainItem } from '~/src/models/thsr';
 import { createContext } from '~/src/server/trpc/context';
 import { appRouter } from '~/src/server/trpc/router/_app';
 import type { RouterInputs } from '~/src/utils/trpc';
-
-const columns = [
-  {
-    label: '出發時間',
-    key: 'DepartureTime',
-  },
-  {
-    label: '到達時間',
-    key: 'DestinationTime',
-  },
-  {
-    label: '行車時間',
-    key: 'Duration',
-  },
-  {
-    label: '車次',
-    key: 'TrainNumber',
-  },
-] as const;
 
 export type SearchPageQuery = Omit<
   RouterInputs['time']['search'],
@@ -80,75 +52,6 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   };
 }
 
-type SearchTableRowProps = {
-  trainItem: TrainItem;
-};
-
-function SearchTableRow({ trainItem }: SearchTableRowProps) {
-  const [isStationInfoOpen, toggleIsStationInfoOpen] = useReducer(
-    (prev) => !prev,
-    false,
-  );
-
-  const discount = trainItem.Discount.map(
-    (item) => `${item.Name} ${item.Value}`,
-  ).join('，');
-  const paddingStyle = discount || trainItem.Note ? { paddingBottom: 30 } : {};
-
-  return (
-    <>
-      <TableRow
-        hover
-        role="checkbox"
-        sx={{ position: 'relative' }}
-        component={Box}
-        onClick={toggleIsStationInfoOpen}
-      >
-        <TableCell
-          align="center"
-          sx={{ position: 'relative' }}
-          style={paddingStyle}
-        >
-          {trainItem[columns[0].key]}
-          <Box
-            sx={{
-              position: 'absolute',
-              pl: 2,
-              pt: 0.5,
-              whiteSpace: 'nowrap',
-            }}
-          >
-            {discount && <Typography variant="body2">{discount}</Typography>}
-            {trainItem.Note && (
-              <Typography variant="body2">{trainItem.Note}</Typography>
-            )}
-          </Box>
-        </TableCell>
-        {columns.slice(1).map((column) => (
-          <TableCell key={column.key} align="center" style={paddingStyle}>
-            {trainItem[column.key]}
-          </TableCell>
-        ))}
-      </TableRow>
-      <TableCell
-        style={{ paddingBottom: 0, paddingTop: 0 }}
-        colSpan={columns.length}
-      >
-        <Collapse in={isStationInfoOpen} timeout="auto" unmountOnExit>
-          <Box sx={{ m: 1, display: 'flex' }}>
-            {trainItem.StationInfo.map(
-              (station) =>
-                station.Show && (
-                  <Box key={station.StationNo}>{station.StationName}</Box>
-                ),
-            )}
-          </Box>
-        </Collapse>
-      </TableCell>
-    </>
-  );
-}
-
 function SearchPage({
   timeSearchData,
   errorMessage,
@@ -163,7 +66,7 @@ function SearchPage({
           position: 'relative',
           pr: 1,
           py: 1,
-          gap: 1,
+          gap: 2,
         }}
       >
         <IconButton
@@ -171,39 +74,107 @@ function SearchPage({
           LinkComponent={Link}
           href="/time"
         >
-          <ArrowBackOutlinedIcon />
+          <ArrowBackRoundedIcon />
         </IconButton>
-        <Typography>{timeSearchData?.Title.StartStationName}</Typography>
-        <DoubleArrowIcon />
-        <Typography>{timeSearchData?.Title.EndStationName}</Typography>
+        <Typography variant="h5">
+          {timeSearchData?.Title.StartStationName}
+        </Typography>
+        <KeyboardDoubleArrowRightRoundedIcon fontSize="large" />
+        <Typography variant="h5">
+          {timeSearchData?.Title.EndStationName}
+        </Typography>
       </Box>
-      <Typography variant="body2" sx={{ ml: 2 }}>
+      <Typography variant="body2" sx={{ ml: 2.5 }}>
         {timeSearchData?.Title.TitleSplit2}
       </Typography>
       {errorMessage && <Typography>{errorMessage}</Typography>}
-      {timeSearchData && (
-        <TableContainer sx={{ maxHeight: '100%' }}>
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow>
-                {columns.map((column) => (
-                  <TableCell key={column.label} align="center">
-                    {column.label}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {timeSearchData.TrainItem.map((trainItem) => (
-                <SearchTableRow
-                  trainItem={trainItem}
-                  key={trainItem.TrainNumber}
-                />
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      )}
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 1,
+          pt: 1,
+          px: 2,
+          pb: 2,
+          overflow: 'auto',
+          height: '100%',
+        }}
+      >
+        {timeSearchData &&
+          timeSearchData.TrainItem.map((trainItem) => {
+            const discount = trainItem.Discount.map(
+              (item) => `${item.Name} ${item.Value}`,
+            ).join('，');
+            return (
+              <Box
+                key={trainItem.TrainNumber}
+                sx={{
+                  bgcolor: (theme) => theme.palette.grey[100],
+                  borderRadius: 2,
+                }}
+              >
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    pl: 2,
+                    pr: 1.5,
+                    pt: 1,
+                    alignItems: 'center',
+                  }}
+                >
+                  <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+                    <Typography variant="h6">
+                      {trainItem.DepartureTime}
+                    </Typography>
+                    <EastRoundedIcon />
+                    <Typography variant="h6">
+                      {trainItem.DestinationTime}
+                    </Typography>
+                  </Box>
+                  <Box sx={{ display: 'flex', gap: 1 }}>
+                    <ScheduleRoundedIcon fontSize="small" />
+                    <Typography variant="body2">
+                      {trainItem.Duration}
+                    </Typography>
+                  </Box>
+                  <Box sx={{ display: 'flex' }}>
+                    <DirectionsTransitRoundedIcon />
+                    <Typography>{trainItem.TrainNumber}</Typography>
+                  </Box>
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    borderTop: (theme) =>
+                      `1px solid ${theme.palette.grey[400]}`,
+                    pl: 2,
+                    px: 1.5,
+                    pt: 0.5,
+                    pb: 1,
+                  }}
+                >
+                  <Typography variant="body2" sx={{ ml: 1 }}>
+                    {discount}
+                  </Typography>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      gap: 0.5,
+                      alignItems: 'center',
+                    }}
+                  >
+                    <IconButton sx={{ p: 0 }}>
+                      <RouteRoundedIcon fontSize="small" />
+                    </IconButton>
+                    <Typography variant="body2">停靠站</Typography>
+                  </Box>
+                </Box>
+              </Box>
+            );
+          })}
+      </Box>
     </>
   );
 }
